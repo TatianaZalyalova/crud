b731ae1fecc8b571ddc4fe8a8fb23555e9025b48
TatianaZalyalova <39691436+TatianaZalyalova@users.noreply.github.com>
2022-01-27 20:01:05 +0300

    create class Crud

---
+++ src/Crud.ts
@@ -0,0 +1,23 @@
+export interface IData {
+  [index: string]: {
+    task: string;
+    date?: string;
+    tags?: string[];
+  };
+}
+
+export abstract class Crud {
+  abstract create(
+    key: string,
+    data: string,
+    path: string
+  ): Promise<string | null>;
+  abstract read(id: string, path: string): Promise<IData>;
+  abstract updated(updates: IData, path?: string): Promise<void>;
+  abstract del(id: string, path: string): Promise<void>;
+  abstract filter(
+    folder: string,
+    keyFilter: string,
+    value: string
+  ): Promise<IData>;
+}
